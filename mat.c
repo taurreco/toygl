b2d416e951f77b73ee76aefb0504813bff97f90e
Tomas Dougan <tomasdougan@gmail.com>
2026-06-25 05:38:04 -0400

    [mat] Enforce 80 col limit

--- mat.c
+++ mat.c
@@ -11,22 +11,38 @@ void
 matmul(struct mat4* a, struct mat4* b)
 {
     struct mat4 tmp;
-    tmp.e00 = a->e00 * b->e00 + a->e01 * b->e10 + a->e02 * b->e20 + a->e03 * b->e30;
-    tmp.e01 = a->e00 * b->e01 + a->e01 * b->e11 + a->e02 * b->e21 + a->e03 * b->e31; 
-    tmp.e02 = a->e00 * b->e02 + a->e01 * b->e12 + a->e02 * b->e22 + a->e03 * b->e32; 
-    tmp.e03 = a->e00 * b->e03 + a->e01 * b->e13 + a->e02 * b->e23 + a->e03 * b->e33; 
-    tmp.e10 = a->e10 * b->e00 + a->e11 * b->e10 + a->e12 * b->e20 + a->e13 * b->e30; 
-    tmp.e11 = a->e10 * b->e01 + a->e11 * b->e11 + a->e12 * b->e21 + a->e13 * b->e31; 
-    tmp.e12 = a->e10 * b->e02 + a->e11 * b->e12 + a->e12 * b->e22 + a->e13 * b->e32; 
-    tmp.e13 = a->e10 * b->e03 + a->e11 * b->e13 + a->e12 * b->e23 + a->e13 * b->e33; 
-    tmp.e20 = a->e20 * b->e00 + a->e21 * b->e10 + a->e22 * b->e20 + a->e23 * b->e30; 
-    tmp.e21 = a->e20 * b->e01 + a->e21 * b->e11 + a->e22 * b->e21 + a->e23 * b->e31; 
-    tmp.e22 = a->e20 * b->e02 + a->e21 * b->e12 + a->e22 * b->e22 + a->e23 * b->e32; 
-    tmp.e23 = a->e20 * b->e03 + a->e21 * b->e13 + a->e22 * b->e23 + a->e23 * b->e33; 
-    tmp.e30 = a->e30 * b->e00 + a->e31 * b->e10 + a->e32 * b->e20 + a->e33 * b->e30; 
-    tmp.e31 = a->e30 * b->e01 + a->e31 * b->e11 + a->e32 * b->e21 + a->e33 * b->e31; 
-    tmp.e32 = a->e30 * b->e02 + a->e31 * b->e12 + a->e32 * b->e22 + a->e33 * b->e32; 
-    tmp.e33 = a->e30 * b->e03 + a->e31 * b->e13 + a->e32 * b->e23 + a->e33 * b->e33;
+    tmp.e00 =   a->e00 * b->e00 + a->e01 * b->e10 + 
+                a->e02 * b->e20 + a->e03 * b->e30;
+    tmp.e01 =   a->e00 * b->e01 + a->e01 * b->e11 +
+                a->e02 * b->e21 + a->e03 * b->e31; 
+    tmp.e02 =   a->e00 * b->e02 + a->e01 * b->e12 +
+                a->e02 * b->e22 + a->e03 * b->e32; 
+    tmp.e03 =   a->e00 * b->e03 + a->e01 * b->e13 +
+                a->e02 * b->e23 + a->e03 * b->e33; 
+    tmp.e10 =   a->e10 * b->e00 + a->e11 * b->e10 +
+                a->e12 * b->e20 + a->e13 * b->e30; 
+    tmp.e11 =   a->e10 * b->e01 + a->e11 * b->e11 +
+                a->e12 * b->e21 + a->e13 * b->e31; 
+    tmp.e12 =   a->e10 * b->e02 + a->e11 * b->e12 +
+                a->e12 * b->e22 + a->e13 * b->e32; 
+    tmp.e13 =   a->e10 * b->e03 + a->e11 * b->e13 +
+                a->e12 * b->e23 + a->e13 * b->e33; 
+    tmp.e20 =   a->e20 * b->e00 + a->e21 * b->e10 +
+                a->e22 * b->e20 + a->e23 * b->e30; 
+    tmp.e21 =   a->e20 * b->e01 + a->e21 * b->e11 +
+                a->e22 * b->e21 + a->e23 * b->e31; 
+    tmp.e22 =   a->e20 * b->e02 + a->e21 * b->e12 +
+                a->e22 * b->e22 + a->e23 * b->e32; 
+    tmp.e23 =   a->e20 * b->e03 + a->e21 * b->e13 +
+                a->e22 * b->e23 + a->e23 * b->e33; 
+    tmp.e30 =   a->e30 * b->e00 + a->e31 * b->e10 +
+                a->e32 * b->e20 + a->e33 * b->e30; 
+    tmp.e31 =   a->e30 * b->e01 + a->e31 * b->e11 +
+                a->e32 * b->e21 + a->e33 * b->e31; 
+    tmp.e32 =   a->e30 * b->e02 + a->e31 * b->e12 +
+                a->e32 * b->e22 + a->e33 * b->e32; 
+    tmp.e33 =   a->e30 * b->e03 + a->e31 * b->e13 +
+                a->e32 * b->e23 + a->e33 * b->e33;
     *a = tmp;
 };
 
